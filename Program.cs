09bc592d0acd65c4d4b55a9e472ee1636a4d2a79
tcantello <tcantello@my.lonestar.edu>
2022-03-30 00:43:21 -0500

    033022 0043 update

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@
 Program - week9Assignment
 Programer - Toby Cantello
 Date Created - 3/25/2022
-Last Date Updated - 3/25/2022
+Last Date Updated - 3/30/2022
 */
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.IO;
 
 namespace week9Assignment
@@ -16,32 +16,52 @@ namespace week9Assignment
 
     class Program
     { 
-        static bool UserInput(out string userInput)
+        static bool UserPath(out string validInput, string userInput)
         {
+            validInput = null;
+            var pathChecker = new Regex(@"\.txt$");
 
-            Console.WriteLine("Please enter the path to the text file you want to open.");
-            userInput = Console.ReadLine();
+            if (pathChecker.IsMatch(userInput))
+            {
+                Console.WriteLine("File path is vaild");
+            }
+            else
+            {
+                Console.WriteLine("You didn't enter a path to a text file");
+                return false;
+            }
 
-            // regex checking
-
-            // if regex is match
             return true;
         }
         static void ReadFile(string input)
         {
-            var sr = new StreamReader(input);
-            Console.WriteLine(sr.ReadLine());
-        }   
-
-        static void Main(string[] args)
-        {
-            string input = "";
-            if(UserInput(out input))
-            { 
-                ReadFile(input); 
+            try
+            {
+                StreamReader sr = new StreamReader(input);
+                Console.WriteLine("Opening the file...");
+                Console.WriteLine();
+                Console.WriteLine(sr.ReadToEnd());
             }
 
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Unfortunately, the file does not exist.");
+            }
+        }   
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            string input = "";
 
+            Console.WriteLine("Please enter the path to the text file you want to open.");
+            userInput = @"" + Console.ReadLine();
+            Console.WriteLine();
+
+
+            if(UserPath(out string validInput, userInput))
+            {
+                ReadFile(userInput);
+            }
         }
     }
 }
